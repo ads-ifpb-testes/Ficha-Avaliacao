f46255d64f37095659e6f0051ff2360cc6b2b4b4
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2019-01-24 10:47:34 -0300

    Teste - método de teste validarFicha CT-018

--- src/test/java/test/FichaTest.java
+++ src/test/java/test/FichaTest.java
@@ -34,26 +34,6 @@ public class FichaTest {
     public void adicionarFichas() {
 //    	Ficha válida
     	fichaAvaliacaoDAO.salvarFicha(new FichaAvaliacao("José","Domingos","Fisioterapeuta","Traumato","111.111.111-11",19,"Masculino","Condomalacia Patelar","Dor Constante","Melhoria no Andar",LocalDate.now()));
-//    	Ficha Campos preenchidos e incorretos
-    	fichaAvaliacaoDAO.salvarFicha(new FichaAvaliacao("Carlos","Pedro","Fisioterapeuta","Traumato","153.181-78",19,"Masculino","Condomalacia Patelar","Dor Constante","Melhoria no Andar",LocalDate.of(1955, Month.DECEMBER, 10)));
-//    	Campo obrigatório omitido
-    	fichaAvaliacaoDAO.salvarFicha(new FichaAvaliacao("Rian","","Fisioterapeuta","Traumato","153.181-78",19,"Masculino","Condomalacia Patelar","Dor Constante","Melhoria no Andar",LocalDate.now()));
-    }
-
-    public FichaAvaliacao fichaFake(){
-        FichaAvaliacao ficha = new FichaAvaliacao();
-        ficha.setNome("Raul");
-        ficha.setNomeDoProfissional("Dra Ruthe");
-        ficha.setGraduacao("Fisioterapeuta");
-        ficha.setEspecializacao("Traumato");
-        ficha.setCpf("111-111-111-11");
-        ficha.setIdade(19);
-        ficha.setSexo("Masculino");
-        ficha.setDiagnostico("Condomalacia Patelar");
-        ficha.setQueixaPrincipal("Dor constante");
-        ficha.setEvolucao("Melhoria no Andar");
-        ficha.setDataConsulta(LocalDate.now());
-        return ficha;
     }
 
 
@@ -65,13 +45,23 @@ public class FichaTest {
     
     @Test
     public void validarFicha() throws CpfInvalidoException, DataConsultaException, CampoVazioException {
-//    	CT-0
+//    	Ficha Campos preenchidos e incorretos
+    	FichaAvaliacao carlos = new FichaAvaliacao("Carlos","Pedro","Fisioterapeuta","Traumato","153.181-78",19,"Masculino","Condomalacia Patelar","Dor Constante","Melhoria no Andar",LocalDate.of(1955, Month.DECEMBER, 10));
+//    	Campo obrigatório omitido
+    	FichaAvaliacao rian = new FichaAvaliacao("Rian","","Fisioterapeuta","Traumato","435.153.181-78",19,"Masculino","Condomalacia Patelar","Dor Constante","Melhoria no Andar",LocalDate.now());
+    	FichaAvaliacao vazio = new FichaAvaliacao("","Alfonso","Pediatra","Imunologia","486.153.181-78",19,"Masculino","Gripe","","",LocalDate.now());
+    	FichaAvaliacao rita = new FichaAvaliacao("Rita","Alfonso","Pediatra","Imunologico","",19,"Feminino","Gripe","","",LocalDate.now());
+    	FichaAvaliacao flavia = new FichaAvaliacao("Flavia","Alfonso","Pediatra","Imunologico","765.656.651-89",19,"Feminino","","","",LocalDate.now());
+//    	CT-016
     	FichaAvaliacao ficha = new FichaAvaliacao("João","Paulo","Fisioterapeuta","Traumato","112.112.112-12",22,"Masculino","Condomalacia Patelar","Não consegue andar","Piora no Andar",LocalDate.now());
     	Assert.assertTrue(fichaAvaliacaoDAO.validarFicha(ficha));
-//    	CT-0
-    	Assert.assertFalse(fichaAvaliacaoDAO.validarFicha(fichaAvaliacaoDAO.listarFichas().get(1)));
-//    	CT-0
-    	Assert.assertFalse(fichaAvaliacaoDAO.validarFicha());
+//    	CT-017
+    	Assert.assertFalse(fichaAvaliacaoDAO.validarFicha(carlos));
+//    	CT-018
+    	Assert.assertFalse(fichaAvaliacaoDAO.validarFicha(rian));
+    	Assert.assertFalse(fichaAvaliacaoDAO.validarFicha(vazio));
+    	Assert.assertFalse(fichaAvaliacaoDAO.validarFicha(rita));
+    	Assert.assertFalse(fichaAvaliacaoDAO.validarFicha(flavia));
     }
 
 }
