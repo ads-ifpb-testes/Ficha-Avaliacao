4f4425ded7f631fcf3a7c19100f76ecf0d31fe32
Leanderson-Coelho <leanderson.coelhoif@gmail.com>
2019-02-25 21:20:51 -0300

    Teste - Modificações para o teste AgendaTest
    Modificações para corrigir o teste da classe AgendaTest.

--- src/test/java/test/AgendaTest.java
+++ src/test/java/test/AgendaTest.java
@@ -1,12 +1,15 @@
 package test;
 
 import static org.mockito.Mockito.when;
+
+import java.time.LocalDate;
 import java.time.LocalTime;
 import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import Control.Agenda;
+import Model.Consulta;
 import Model.Profissional;
 import dao.ProfissionalDAO;
 import enumeration.DIA;
@@ -26,12 +29,12 @@ public class AgendaTest {
 	public void agendarConsultaTest() {
 //    	CT-022
     	when(profissionalDAO.buscar("Paulo")).thenReturn(new Profissional("Paulo","034.342.523-24","Medicina","Pediatra",DIA.QUARTA,LocalTime.of(07, 00),LocalTime.of(11, 00)));
-    	Assert.assertTrue(agenda.agendarConsulta(profissionalDAO.buscar("Paulo"),LocalTime.of(8, 00),LocalTime.of(9, 30),DIA.QUARTA));
+    	Assert.assertTrue(Agenda.agendarConsulta(new Consulta(profissionalDAO.buscar("Paulo"),LocalTime.of(8, 00),LocalTime.of(9, 30),DIA.QUARTA,LocalDate.of(2019, 5, 10))));
 //    	CT-023
-    	Assert.assertFalse(agenda.agendarConsulta(profissionalDAO.buscar("Paulo"),LocalTime.of(8, 30),LocalTime.of(9, 15),DIA.QUARTA));
-    	Assert.assertFalse(agenda.agendarConsulta(profissionalDAO.buscar("Paulo"),LocalTime.of(7, 30),LocalTime.of(8, 30),DIA.QUARTA));
-    	Assert.assertFalse(agenda.agendarConsulta(profissionalDAO.buscar("Paulo"),LocalTime.of(9, 20),LocalTime.of(10, 00),DIA.QUARTA));
+    	Assert.assertFalse(Agenda.agendarConsulta(new Consulta(profissionalDAO.buscar("Paulo"),LocalTime.of(8, 30),LocalTime.of(9, 15),DIA.QUARTA,LocalDate.of(2019, 5, 10))));
+    	Assert.assertFalse(Agenda.agendarConsulta(new Consulta(profissionalDAO.buscar("Paulo"),LocalTime.of(7, 30),LocalTime.of(8, 30),DIA.QUARTA,LocalDate.of(2019, 5, 10))));
+    	Assert.assertFalse(Agenda.agendarConsulta(new Consulta(profissionalDAO.buscar("Paulo"),LocalTime.of(9, 20),LocalTime.of(10, 00),DIA.QUARTA,LocalDate.of(2019, 5, 10))));
 //    	CT-024
-    	Assert.assertFalse(agenda.agendarConsulta(profissionalDAO.buscar("Paulo"),LocalTime.of(10, 10),LocalTime.of(10, 50),DIA.SEGUNDA));
+    	Assert.assertFalse(Agenda.agendarConsulta(new Consulta(profissionalDAO.buscar("Paulo"),LocalTime.of(10, 10),LocalTime.of(10, 50),DIA.SEGUNDA,LocalDate.of(2019, 5, 10))));
 	}
 }
